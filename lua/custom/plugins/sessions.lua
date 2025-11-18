bdf09967c113887c7a50e4fcb3b1badabb25331e
Martin Raspaud <martin.raspaud@smhi.se>
2025-11-18 19:10:47 +0100

    First draft

---
+++ lua/custom/plugins/sessions.lua
@@ -0,0 +1,14 @@
+vim.o.sessionoptions = "blank,buffers,curdir,help,tabpages,winsize,winpos,terminal,localoptions"
+
+return {
+  "rmagatti/auto-session",
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+    -- log_level = 'debug',
+  },
+}
